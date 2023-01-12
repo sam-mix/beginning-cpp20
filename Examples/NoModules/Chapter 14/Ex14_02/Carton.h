f359872350a7f38914db62443373b7c5c6811254
Sam Mix <1045262335@qq.com>
2023-01-12 12:45:35 +0000

    edit complete

--- Examples/NoModules/Chapter 14/Ex14_02/Carton.h
+++ Examples/NoModules/Chapter 14/Ex14_02/Carton.h
@@ -4,42 +4,45 @@
 
 #include <string>
 #include <string_view>
+
 #include "Box.h"
 
-class Carton : public Box
-{
-public:
+class Carton : public Box {
+ public:
   Carton() { std::cout << "Carton() called.\n"; }
 
-  explicit Carton(std::string_view material) : m_material{material}
-  { std::cout << "Carton(string_view) called.\n"; }
+  explicit Carton(std::string_view material) : m_material{material} {
+    std::cout << "Carton(string_view) called.\n";
+  }
 
-  Carton(double side, std::string_view material) : Box{side}, m_material{material}
-  { std::cout << "Carton(double,string_view) called.\n"; }
+  Carton(double side, std::string_view material)
+      : Box{side}, m_material{material} {
+    std::cout << "Carton(double,string_view) called.\n";
+  }
 
   Carton(double l, double w, double h, std::string_view material)
-    : Box{l, w, h}, m_material{material}
-    { std::cout << "Carton(double,double,double,string_view) called.\n"; }  
-/*
-  // This constructor won't compile!
-  Carton::Carton(double l, double w, double h, std::string_view material)
-    : m_length{ l }, m_width{ w }, m_height{ h }, m_material{ material }
-    { std::cout << "Carton(double,double,double,string_view) called.\n"; }
-*/
-/*
-  // Constructor that will compile!
-  Carton::Carton(double l, double w, double h, std::string_view material)
-    : m_material{material}
-  {
-    m_length = l;  // These should normally be initialized in a base class constructor...
-    m_width = w;
-    m_height = h;
+      : Box{l, w, h}, m_material{material} {
     std::cout << "Carton(double,double,double,string_view) called.\n";
   }
-*/
+  /*
+    // This constructor won't compile!
+    Carton::Carton(double l, double w, double h, std::string_view material)
+      : m_length{ l }, m_width{ w }, m_height{ h }, m_material{ material }
+      { std::cout << "Carton(double,double,double,string_view) called.\n"; }
+  */
+  /*
+    // Constructor that will compile!
+    Carton::Carton(double l, double w, double h, std::string_view material)
+      : m_material{material}
+    {
+      m_length = l;  // These should normally be initialized in a base class
+    constructor... m_width = w; m_height = h; std::cout <<
+    "Carton(double,double,double,string_view) called.\n";
+    }
+  */
 
-private:
-  std::string m_material {"Cardboard"};
+ private:
+  std::string m_material{"Cardboard"};
 };
 
 #endif
